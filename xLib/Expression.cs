0593a58baf27b09732ab721c9225b83416ef3a30
HieuDo <minhhieu76qng@gmail.com>
2019-11-26 11:17:57 +0700

    init project

---
+++ xLib/Expression.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xLib
+{
+    interface Expression
+    {
+        public double evalute(double x);
+        public Expression derive();
+        public string toString(Notation ntn);
+    }
+}
---
+++ xLib/Notation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xLib
+{
+    interface Notation
+    {
+        public string arrange(string op, string operand1, string operand2);
+    }
+}
